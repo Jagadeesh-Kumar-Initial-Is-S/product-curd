aa9953899de6853facd7d938aebe8b14148b4fcf
Jagadeesh Kumar . S <jagadeesh_2k17@proton.me>
2022-12-09 14:04:30 +0530

    Second commit to master.

--- src/App.js
+++ src/App.js
@@ -42,26 +42,30 @@ function App() {
   //   },
   // ];
   return (
-    <div className="App">
-      <NavBar />
-      <Switch>
-        <Route path="/" exact>
-          <Home />
-        </Route>
-        <Route path="/add" exact>
-          <AddProduct />
-        </Route>
-        <Route path="/dashboard" exact>
-          <Dashboard />
-        </Route>
-        <Route path="/edit/:id" exact>
-          <EditProduct />
-        </Route>
-        <Route path="**" exact>
-          <h2>404</h2>
-        </Route>
-      </Switch>
-    </div>
+    <><div class="header">
+      <a href='https://github.com/Jagadeesh-Kumar-Initial-Is-S' target='_blank'>Submitted and coded by Jagadeesh Kumar . S, you may send mail to my email address which is jagadeesh_2k17@proton.me, you may contribute some money to my Indian Unified Payment Interface (UPI) which is jagadeesh-kumar@ybl .</a>
+    </div><div className="App">
+        <NavBar />
+        <Switch>
+          <Route path="/" exact>
+            <Home />
+          </Route>
+          <Route path="/add" exact>
+            <AddProduct />
+          </Route>
+          <Route path="/dashboard" exact>
+            <Dashboard />
+          </Route>
+          <Route path="/edit/:id" exact>
+            <EditProduct />
+          </Route>
+          <Route path="**" exact>
+            <h2>404</h2>
+          </Route>
+        </Switch>
+      </div><div class="footer">
+        <a href='https://github.com/Jagadeesh-Kumar-Initial-Is-S' target='_blank'>Submitted and coded by Jagadeesh Kumar . S, you may send mail to my email address which is jagadeesh_2k17@proton.me, you may contribute some money to my Indian Unified Payment Interface (UPI) which is jagadeesh-kumar@ybl .</a>
+      </div></>
   );
 }
 
